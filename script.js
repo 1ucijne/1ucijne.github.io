2d00e7dc4c08e353a99b9e437e9dce81fb51851e
Lucine Sokola <ls63@fordham.edu>
2025-03-18 22:28:33 -0400

    Initial commit or update

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function showImage(src) {
+    const displayImage = document.getElementById('display');
+    displayImage.src = src;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("Artist Portfolio Loaded");
+    document.querySelectorAll(".gallery-item").forEach(item => {
+        item.addEventListener("click", function() {
+            showImage(this.src);
+        });
+    });
 });
